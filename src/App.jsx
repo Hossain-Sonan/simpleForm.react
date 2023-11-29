6c4fa97b77adf1ea786e34419a5b19d647047dc8
Alaul1988 Sunan <alaul1988sunan@gmail.com>
2023-11-28 22:59:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,22 @@
+
+
+const SimpleForm = () => {
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        console.log("form submitted");
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" />
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
+            
+        </div>
+    );
+};
+
+export default SimpleForm;
